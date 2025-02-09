0e9f512cbd7ec825885a7738fd6662cb45175ccc
Aisys <aisysaidan@gmail.com>
2025-02-08 23:52:28 -0600

    src

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-const userRoutes = require('./routes/userRoutes');
-const taskRoutes = require('./routes/taskRoutes');
+const userRoutes = require('./src/routes/userRoutes');
+const taskRoutes = require('./src/routes/taskRoutes');
 
 app.use(cors());
 app.use(bodyParser.json());
